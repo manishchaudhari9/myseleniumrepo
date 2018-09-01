d1eb4b57739d6ec138728b1020763396e7bd2fdc
manishchaudhari9 <manish.chaudhari9@gmail.com>
2018-09-01 12:57:37 -0400

    Updated

--- CoreFramework/src/test/java/com/selenium/testcases/portfoliosuite/LoginTest.java
+++ CoreFramework/src/test/java/com/selenium/testcases/portfoliosuite/LoginTest.java
@@ -13,10 +13,8 @@ import com.selenium.util.DataUtil;
 public class LoginTest extends BaseTest {
 	
 	@Test(dataProvider="getData")
-	public void loginTest(Hashtable<String,String> data) throws Exception{
-		
+	public void loginTest(Hashtable<String,String> data) throws Exception{		
 		test.log(Status.INFO, "Starting "+ testName);
-
 		if(DataUtil.isSkip(testName, xls) ||data.get(Constants.RUNMODE_COL).equals(Constants.RUNMODE_NO)){
 			test.log(Status.SKIP, "Runmode is set to NO");
 			throw new SkipException("Runmode is set to NO");
